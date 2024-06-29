844819a0c8cb25c02df4e2a1b22d76cc3540e573
raghu <anand@digikit.in>
2024-06-29 17:46:24 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import "swiper/css/navigation";
 const Header = dynamic(() => import("@/components/Layout/Header/Header"), {
   ssr: false,
 });
-
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Footer from "@/components/Layout/Footer/Footer";
 import Whatsapp from "@/components/Ui/SideNavWidgets/Whatsapp/Whatsapp";
 import Script from "next/script";
@@ -27,23 +27,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KWMMBKVS');
-          `}
-        </Script>
-        <link rel="icon" type="image/x-icon" href="/assets/favicon.png"></link>
+        <link rel="icon" type="image/x-icon" href="/assets/favicon.png"></link>{" "}
       </head>
       <body className={inter.className}>
-        <noscript
+        <GoogleAnalytics gaId="AW-986592530" />{" "}
+        <GoogleTagManager gtmId="G-T9ZNQ43GTF" />
+        {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWMMBKVS" height="0" width="0" style="display: none; visibility: hidden;" />`,
           }}
-        />
+        /> */}
         <Header />
         {children}
         <Footer />
--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Container from "@/components/Layout/Container";
 import styles from "./pages.module.css";
+import Script from "next/script";
 
 export default function page() {
   return (
     <>
+      <Script id="gtag">
+        {`  gtag('event', 'conversion', {'send_to': 'AW-986592530/lhTZCKL8_r0ZEJLquNYD'});`}
+      </Script>
       <section className={styles.wrapper}>
         <section className={styles.content}>
           <Container width>
